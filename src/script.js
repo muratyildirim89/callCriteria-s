2a298cd9d3acb3e40e58f6c43995254298bc828f
Mirat YILDIRIM <77025193+muratyildirim89@users.noreply.github.com>
2022-12-06 08:08:41 +0300

    Add files via upload
    First Commit

---
+++ src/script.js
@@ -0,0 +1,15 @@
+function darkMode() {
+    document.querySelector(".navApp").classList.toggle("navDark");
+    document.querySelector(".bodyLightThema").classList.toggle("bodyDarkThema");
+    document.querySelector(".tablePersonal").classList.toggle("table-danger");
+    document.querySelector(".table").classList.toggle("table-dark");
+    document.querySelector(".aside").classList.toggle("asideDark");
+    
+    
+
+    /*document.querySelector('.tableShow').classList.toggle("table-dark");
+    document.querySelector('.tablePersonal').classList.toggle("table-dark");
+    document.querySelector('.workArea1').classList.toggle("workDark1");
+    document.querySelector('.workArea2').classList.toggle("workDark2");  */  
+
+}
